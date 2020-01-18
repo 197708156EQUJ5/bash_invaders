57b6a2878208d36045f826eef9d22c5a7fbdcc19
197708156EQUJ5 <ggraves73@gmail.com>
2020-01-18 14:28:00 -0500

    [BHINV-1] Initial work on moving enemies

--- src/invaders.sh
+++ src/invaders.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+## ----------------------------------------------
+## @File: invaders.sh
 source lib/utils
 source lib/cursor_move
 source lib/title
@@ -11,7 +12,15 @@ SCORE=0
 HIGH=0
 LIVES=3
 
+function showDocs() {
+  grep -Ir '^##' . | sed -e 's/^.*##//' 1>&2
+}
+
+##
+## @Name: main
+## @desc: runs the bash Invaders Game
 function main() {
+    touch ~/temp/bhinv-debug
     hide_cursor
     init_screen
     show_title
@@ -22,6 +31,7 @@ function main() {
     create_enemy 4 2
     #update_pos
     stty -echo
+    tick=0
     while :
     do
         read -t 0.01 -n 1 key
@@ -38,9 +48,22 @@ function main() {
         fi
         draw_bullet
         sleep 0.04
+        ((tick+=1))
+        if [ $((tick % 10)) == 0 ]; then
+            debug $tick
+            move_enemy_right
+        fi
     done
     stty echo
     show_cursor
+    rm ~/temp/bhinv-debug
 }
 
+if [[ "$1" = "list_func" ]]; then
+    showDocs
+    exit
+fi
+
 main
+
+##
